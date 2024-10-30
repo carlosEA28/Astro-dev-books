c770d0593f3bd716ec0d597f4a9a51dbda242419
carlosEA28 <carloseaduarte28@gmail.com>
2024-10-29 21:35:59 -0300

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
+  adapter: vercel(),
 });
